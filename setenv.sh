325d74a61a0adc3bb4baabdc371212e60655316f
Hoa Lu <hoalu.vn@gmail.com>
2015-02-07 00:10:11 +0700

    update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,2 +1,14 @@
-export PROJ_ROOT=/home/htlu/works/arm
-export PATH=~/tools/gcc/bin:$PATH
+#export PATH=~/tools/gcc/bin:$PATH
+
+#export PROJ_ROOT=/home/htlu/works/arm
+loop_cnt=0
+cur_dir=`pwd`
+while [ ! -e $cur_dir/.proj_root ]; do
+  cur_dir=`echo $cur_dir | sed 's/\/[^/]*$//g'`
+  loop_cnt=$loop_cnt + 1
+  if [$loop_cnt > 50]; then
+    echo "Error: Cannot find .proj_root -- please make sure you source the script under proper $PROJ_NAME svn tree!";
+    exit 1
+  fi
+done
+export PROJ_ROOT=$cur_dir
