39e070844364e2cbe7489acd6b992008c194080b
Hoa Lu <htlu@apmlap.(none)>
2015-04-02 02:04:24 +0700

    compile clean

--- prj/STM32F103C6T8-GPIO-Toggle/main.c
+++ prj/STM32F103C6T8-GPIO-Toggle/main.c
@@ -84,20 +84,23 @@ int main(void)
   SystemClock_Config();
   
   /* -1- Enable GPIO Clock (to be able to program the configuration registers) */
-  LED2_GPIO_CLK_ENABLE();
+  //LED2_GPIO_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /* -2- Configure IO in output push-pull mode to drive external LEDs */
   GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull  = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
 
-  GPIO_InitStruct.Pin = LED2_PIN;
-  HAL_GPIO_Init(LED2_GPIO_PORT, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  //HAL_GPIO_Init(LED2_GPIO_PORT, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* -3- Toggle IO in an infinite loop */
   while (1)
   {
-    HAL_GPIO_TogglePin(LED2_GPIO_PORT, LED2_PIN);
+    //HAL_GPIO_TogglePin(LED2_GPIO_PORT, LED2_PIN);
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_9);
     /* Insert delay 100 ms */
     HAL_Delay(100);
   }
