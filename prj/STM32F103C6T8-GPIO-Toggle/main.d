e1439c7365aac547e0061d1501bfae37f107c5ff
Hoa Lu <hlu@dr.com>
2015-04-03 08:03:08 +0700

    add hex and bin format

--- prj/STM32F103C6T8-GPIO-Toggle/main.d
+++ prj/STM32F103C6T8-GPIO-Toggle/main.d
@@ -1,18 +1,18 @@
 main.o: main.c main.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
  stm32f1xx_hal_conf.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/CMSIS/Include/core_cm3.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/CMSIS/Include/core_cmInstr.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/CMSIS/Include/core_cmFunc.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
- /home/htlu/Works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/CMSIS/Include/core_cm3.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ /home/htlu/works/arm/lib/STM32Cube/STM32Cube_FW_F1_V1.0.0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
